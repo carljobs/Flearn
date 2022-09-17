c1a5e5ecc91fa4cf0ce61237d3026cc93d783aa6
carljobs <carljarabelo@gmail.com>
2022-09-17 22:56:31 +0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // User::factory(10)->create();
+        //App\Models\User::factory(50)->hasPosts(10)->create();
+
+        User::factory()->count(50)->hasPosts(10)->create();
+        
+        $this->call([
+            UserSeeder::class,
+            PostSeeder::class
+        ]);
+        
+    }
+}
